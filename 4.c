afb7fd32b05a2c81fcc66ad3d7601d400b9adf22
jananiselvaraju0502-prog <jananiselvaraju0502@gmail.com>
2026-01-02 19:20:48 +0530

    Add files via upload
    a year is a leap year

---
+++ 4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int num1,num2,num3,largest; 
+
+
+    printf("Enter three number:");
+    scanf("%d %d %d",&num1, &num2, &num3);
+        largest = num1;
+    if(num2 > largest) {
+        largest = num2;
+    }
+    if (num3 > largest) {
+        largest = num3;
+    }
+      printf("%d\n", largest);
+   
+    
+    return 0;
+}
