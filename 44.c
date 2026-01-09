91d94316be913049c60ce089e76827788762062c
jananiselvaraju0502-prog <jananiselvaraju0502@gmail.com>
2026-01-09 22:16:31 +0530

    Add files via upload
    find the second largest elements in an array

---
+++ 44.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n, a[100],i,j,temp;
+    scanf("%d", &n);
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    for(int i=0;i<n;i++)
+         for(int j=i+1;j<n;j++)
+        if(a[i] < a[j]){
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    printf("%d", a[1]);
+    return 0;
+}
